209e9253b1e586d88d87009d569225f524df8201
Antonelia Valenti <antonelia.valenti@hotmail.com>
2024-06-13 15:07:59 -0300

    supertest

--- test/dao/supertest.test.js
+++
@@ -1,7 +0,0 @@
-const chai = require("chai");
-const supertest = require("supertest")
-
-let expect = chai.expect;
-const requester = supertest('http://localhost:8080/')
-
-mongoose.connect("mongodb+srv://avalenti3003:Teclado3003@proyectocoder.wcbxmpy.mongodb.net/eccomerce-test")
---
+++ test/dao/supertest.test.mjs
@@ -0,0 +1,41 @@
+import { expect } from "chai"
+import supertest from "supertest"
+import mongoose from "mongoose"
+
+const requester = supertest('http://localhost:8080/')
+
+mongoose.connect("mongodb+srv://avalenti3003:Teclado3003@proyectocoder.wcbxmpy.mongodb.net/eccomerce-test")
+
+describe('Testing ecommerce App', () => {
+    // before
+
+    describe("Testing products Api", () => {
+        // before
+
+        // it 01
+        it("Crear productos: El API POST /api/products/products debe crear un nuevo producto correctamente", async () => {
+            // Given 
+            let mockProduct = {
+                title: 'Test Product 12',
+                price: 10.99,
+                category: 'dulce',
+                stock: 20,
+                owner: new mongoose.Types.ObjectId()
+            };
+
+            // Then
+            const { body } = await requester.post('/api/products/products').send(mockProduct);
+
+            // Assert
+    
+            expect(body).to.have.property('title', mockProduct.title);
+            expect(body).to.have.property('price', mockProduct.price);
+            expect(body).to.have.property('category', mockProduct.category);
+            expect(body).to.have.property('stock', mockProduct.stock);
+            expect(body).to.have.property('owner').that.equals(String(mockProduct.owner)); // Conversión a cadena si es necesario
+        });
+    });
+});
+
+//expect(statusCode).is.eqls(404)
+            //expect(_body.payload).is.ok.and.to.have.property('_id')
